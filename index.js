65a58d9a37ff07925e736c2388903c4af7c36942
Dominic Black <dpblack96@gmail.com>
2022-07-14 10:49:56 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+const email = 'steve@steve.com'
+
+function submitData(name, email) {
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+        "name": name,
+        "email": email
+     })
+    
+  };
+  
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then((response)=> response.json())
+    .then(body => document.body.append(body.id))
+    .catch(response => document.body.append( `
+    <p> ${response.code} </p>
+    <p> ${response.message} </p>
+    `))
+}
+
+
+submitData(name,email)
+
